8997471d4a891eb253538b529cdf67cf9034e49f
Rementeria <p.gonzalez@outlook.com>
2020-09-24 21:25:29 -0300

    added btn css & styles

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,12 +2,28 @@ import React, {useState} from "react";
 import {Link} from "react-router-dom";
 import {MdFingerprint} from "react-icons/md";
 import {FaBars, FaTimes} from "react-icons/fa";
+import { Button } from "./Button";
 
 function Navbar() {
     const [click, setClick] = useState(false);
-    const handleClick = () =>{
-        setClick(!click)
-    }
+    const [button, setButton] = useState(true);
+
+    const handleClick = () =>
+        setClick(!click);
+    
+    const closeMobileMenu = () =>
+        setClick(false);
+
+    const showButton = () =>{
+        if(window.innerWidth <= 960){
+            setClick(false)
+        }else{
+            setButton(true)
+        }
+    };
+
+    window.addEventListener('resize', showButton);
+    
   return (
     <>
       <div className="navbar">
